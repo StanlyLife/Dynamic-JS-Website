3f446459ffbd830b37b5423de6e3654380ebad8b
stanlylife <stianhave@hotmail.com>
2020-04-18 15:21:49 +0200

    added nav

--- main.js
+++ main.js
@@ -59,6 +59,8 @@ function animateSlides() {
 }
 
 let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
@@ -71,8 +73,37 @@ function activeCursor(e) {
   } else {
     mouse.classList.remove("nav-active");
   }
+
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    gsap.to(".title-swipe", 1, { y: "100%" });
+    mouseTxt.innerText = "";
+  }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: 45, y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: -45, y: -5, background: "black" });
+    gsap.to("#logo", 0.5, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: 0, y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: 0, y: 0, background: "white" });
+    gsap.to("#logo", 0.5, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
